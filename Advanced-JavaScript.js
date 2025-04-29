b1bbeadf695fce25b53c9d2cd486a27bab07f961
jubayerfahim264 <hatb922@gmail.com>
2025-04-29 09:36:13 +0600

    Practice

---
+++ Advanced-JavaScript.js
@@ -0,0 +1,19 @@
+//False Condition
+// 0;
+//""
+//null
+//undefined value
+//false
+//=========================//
+//True Condition
+//" " whitespace value
+//"0"
+//true
+//0<1 0 greater then 0 any number
+//"str" String
+let str = "str";
+if (str) {
+  console.log("condition is true");
+} else {
+  console.log("condition is false");
+}
