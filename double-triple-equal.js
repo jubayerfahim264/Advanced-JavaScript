43198904ced13a335b6e0842c7cd3eeb8d7639df
jubayerfahim264 <hatb922@gmail.com>
2025-04-29 10:27:45 +0600

    double and triple equal

---
+++ double-triple-equal.js
@@ -0,0 +1,8 @@
+const fNumber = 20;
+const sNumber = "20";
+if (fNumber === sNumber) {
+  console.log("condition is true");
+} else {
+  console.log("condition is false");
+}
+// == it's means it will check only value & === it's means it will check value with data type.
